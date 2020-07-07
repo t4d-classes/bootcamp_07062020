f4747e7b765b63f1832273f2a5583f441e872137
Eric Greene <eric@t4d.io>
2020-07-07 17:12:21 -0400

    add code demos

--- demo-app/src/components/ColorTool.js
+++ demo-app/src/components/ColorTool.js
@@ -4,16 +4,22 @@ import './ColorTool.css';
 
 export const ColorTool = () => {
 
+  const colors = [
+    { id: 1, name: 'aquamarine', hexcode: '6BCAE2' },
+    { id: 2, name: 'neon pink', hexcode: 'FF6EC7' },
+    { id: 3, name: 'black', hexcode: '000000' },
+    { id: 4, name: 'blue', hexcode: '0000FF' },
+  ];
+
   return (
     <>
       <header>
         <h1>Color Tool</h1>
       </header>
       <ul>
-        <li>aquamarine</li>
-        <li>neon pink</li>
-        <li>black</li>
-        <li>blue</li>
+        {colors.map( color => <li key={color.id}>
+          {color.name}
+        </li>)}
       </ul>
     </>
   );
