9ae7ab9f23f02f636bc03a6e3f512e2be27b8a35
Eric Greene <eric@t4d.io>
2020-07-07 19:40:16 -0400

    add code demos

--- demo-app/src/components/ColorTool.js
+++ demo-app/src/components/ColorTool.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './ColorTool.css';
 
 export const ColorTool = (props) => {
 
+  const [
+    colorForm, // state data
+    setColorForm, // state update and re-render function
+  ] = useState({
+    name: '', hexcode: '',
+  } /* initial state */);
 
+  const change = (e) => {
+
+    setColorForm({
+      // object spread operator
+      ...colorForm, // copy the properties of the original color form object to the new object
+
+      // computed property
+      [ e.target.name ]: e.target.value,
+    });
+
+    // colorForm[e.target.name] = e.target.value;
+
+    // console.log(colorForm);
+
+    // setColorForm(colorForm);
+
+  };
+
+  console.log(colorForm);
 
   return (
     <>
@@ -16,6 +41,16 @@ export const ColorTool = (props) => {
           {color.name}
         </li>)}
       </ul>
+      <form>
+        <div>
+          <label htmlFor="name-input">Name:</label>
+          <input type="text" id="name-input" name="name" value={colorForm.name} onChange={change} />
+        </div>
+        <div>
+          <label htmlFor="hexcode-input">Hexcode:</label>
+          <input type="text" id="hexcode-input" name="hexcode" value={colorForm.hexcode} onChange={change} />
+        </div>
+      </form>
     </>
   );
 
