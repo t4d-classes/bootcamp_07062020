0eb7074e6d58453b2fb470eb2be26fbd81d4baf1
Eric Greene <eric@t4d.io>
2020-07-08 18:46:58 -0400

    add exercise 9 solution

---
+++ demo-app/src/components/CarForm.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+
+import {
+  StringFormControl,
+  NumberFormControl,
+} from './FormControls';
+
+import { valueToNaN } from '../utils';
+
+export const CarForm = ({ buttonText, onSubmitCar }) => {
+
+  const [ carForm, setCarForm ] = useState({
+    make: '',
+    model: '',
+    year: NaN,
+    color: '',
+    price: NaN,
+  });
+
+
+  const change = (e) => {
+    setCarForm({
+      ...carForm,
+      [ e.target.name ]: e.target.type === 'number'
+        ? valueToNaN(e.target.value)
+        : e.target.value,
+    });
+  };
+
+  const submitCar = () => {
+
+    onSubmitCar(carForm);
+
+    setCarForm({
+      make: '',
+      model: '',
+      year: NaN,
+      color: '',
+      price: NaN,
+    });
+
+  };
+
+
+  return (
+    <form>
+      {/*
+        
+        React.createElement(StringFormControl, { 
+          caption: 'Make', name: 'make',
+          value: carForm.make, onChange: change,
+        });
+      
+      */}
+      <StringFormControl caption="Make" name="make"
+        value={carForm.make} onChange={change} />
+      <StringFormControl caption="Model" name="model"
+        value={carForm.model} onChange={change} />
+      <NumberFormControl caption="Year" name="year"
+        value={carForm.year} onChange={change} />
+      <StringFormControl caption="Color" name="color"
+        value={carForm.color} onChange={change} />
+      <NumberFormControl caption="Price" name="price"
+        value={carForm.price} onChange={change} />
+      <button type="button" onClick={submitCar}>{buttonText}</button>
+    </form>
+  )
+
+};
--- demo-app/src/components/CarTool.js
+++ demo-app/src/components/CarTool.js
@@ -1,54 +1,21 @@
 import React, { useState } from 'react';
 
-import { valueToNaN } from '../utils';
 
 import "./CarTool.css";
 
-import {
-  StringFormControl,
-  NumberFormControl,
-} from './FormControls';
 import { ToolHeader } from './ToolHeader';
 import { CarTable } from './CarTable';
-
-
+import { CarForm } from './CarForm';
 
 export const CarTool = (props) => {
 
   const [ cars, setCars ] = useState(props.cars.concat());
 
-  const [ carForm, setCarForm ] = useState({
-    make: '',
-    model: '',
-    year: NaN,
-    color: '',
-    price: NaN,
-  });
-
-  const change = (e) => {
-    setCarForm({
-      ...carForm,
-      [ e.target.name ]: e.target.type === 'number'
-        ? valueToNaN(e.target.value)
-        : e.target.value,
-    });
-  };
-
-  const addCar = () => {
-
+  const addCar = (car) => {
     setCars(cars.concat({
-      ...carForm,
+      ...car,
       id: Math.max(...cars.map(c => c.id), 0) + 1,
     }));
-
-    setCarForm({
-      make: '',
-      model: '',
-      year: NaN,
-      color: '',
-      price: NaN,
-    });
-
   };
 
   return (
@@ -56,27 +23,7 @@ export const CarTool = (props) => {
       <ToolHeader headerText="Car Tool" />
       {/* React.createElement(CarTable, { cars: cars }); */}
       <CarTable cars={cars} />
-      <form>
-        {/*
-          
-          React.createElement(StringFormControl, { 
-            caption: 'Make', name: 'make',
-            value: carForm.make, onChange: change,
-          });
-        
-        */}
-        <StringFormControl caption="Make" name="make"
-          value={carForm.make} onChange={change} />
-        <StringFormControl caption="Model" name="model"
-          value={carForm.model} onChange={change} />
-        <NumberFormControl caption="Year" name="year"
-          value={carForm.year} onChange={change} />
-        <StringFormControl caption="Color" name="color"
-          value={carForm.color} onChange={change} />
-        <NumberFormControl caption="Price" name="price"
-          value={carForm.price} onChange={change} />
-        <button type="button" onClick={addCar}>Add Car</button>
-      </form>
+      <CarForm buttonText="Add Car" onSubmitCar={addCar} />
     </div>
   );
 
