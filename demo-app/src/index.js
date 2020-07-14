383c631673a9fc312789abca8aa97debc4c8a67a
Eric Greene <eric@t4d.io>
2020-07-14 10:55:14 -0400

    add starter project for apollo

--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -1,21 +1,21 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux'; 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'; 
 
-// import { colorToolStore } from './stores/colorToolStore';
-// import { carToolStore } from './stores/carToolStore';
+import { colorToolStore } from './stores/colorToolStore';
+import { carToolStore } from './stores/carToolStore';
 
-// import { ColorToolContainer } from './containers/ColorToolContainer';
-// import { CarToolContainer } from './containers/CarToolContainer';
+import { ColorToolContainer } from './containers/ColorToolContainer';
+import { CarToolContainer } from './containers/CarToolContainer';
 
-// ReactDOM.render(
-//   <>
-//     <Provider store={colorToolStore}>
-//       <ColorToolContainer />
-//     </Provider>
-//     <Provider store={carToolStore}>
-//       <CarToolContainer />
-//     </Provider>
-//   </>,
-//   document.querySelector('#root'),
-// );
+ReactDOM.render(
+  <>
+    <Provider store={colorToolStore}>
+      <ColorToolContainer />
+    </Provider>
+    <Provider store={carToolStore}>
+      <CarToolContainer />
+    </Provider>
+  </>,
+  document.querySelector('#root'),
+);
--- demo-app/src/stores/carToolStore.js
+++ demo-app/src/stores/carToolStore.js
@@ -1,6 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
 import { carReducer } from '../reducers/carToolReducers';
 
-export const carToolStore = createStore(carReducer, composeWithDevTools());
+export const carToolStore = createStore(
+  carReducer,
+  composeWithDevTools(applyMiddleware(thunk)),
+);
