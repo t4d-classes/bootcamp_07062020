5e6b4e2c569d0d575dc04b646a4ab4746706978f
Eric Greene <eric@t4d.io>
2020-07-15 13:31:32 -0400

    add graphql server code

--- apollo-graphql-app-starter/graphql-server/resolvers.js
+++ apollo-graphql-app-starter/graphql-server/resolvers.js
@@ -1,7 +1,14 @@
 import fetch from 'node-fetch';
 
+const authors = [
+  { id: 1, firstName: 'Bob', lastName: 'Smith', age: 34 },
+  { id: 2, firstName: 'Alice', lastName: 'Thompkins', age: 23 },
+  { id: 3, firstName: 'Tim', lastName: 'Johnson', age: 56 },
+];
+
 export const resolvers = {
   Query: {
     message: () => 'Hello World!',
+    authors: () => authors,
   },
 };
--- apollo-graphql-app-starter/graphql-server/schema.js
+++ apollo-graphql-app-starter/graphql-server/schema.js
@@ -1,5 +1,13 @@
 export const typeDefs = `
   type Query {
     message: String
+    authors: [Person]
+  }
+
+  type Person {
+    id: ID
+    firstName: String
+    lastName: String
+    age: Int
   }
 `;
